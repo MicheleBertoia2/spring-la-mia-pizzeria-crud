a58e5335d6a8b527aab60c57504e169cc0c66d96
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-10-06 12:44:34 +0200

    add create and validations

--- src/main/java/org/java/lessons/db/pojo/Pizza.java
+++ src/main/java/org/java/lessons/db/pojo/Pizza.java
@@ -1,10 +1,14 @@
 package org.java.lessons.db.pojo;
 
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
 
 @Entity
 public class Pizza {
@@ -13,9 +17,20 @@ public class Pizza {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@Column( length = 128)
+	@Length (
+				min = 3,
+				max = 128,
+				message = "il nome deve contenere 3-128 caratteri"
+			)
 	private String name;
+	
+	@Min(0)
 	private double price;
+	
 	private String image;
+	
+	@Column(columnDefinition = "text")
 	private String ingredients;
 	
 	public Pizza() {}
--- src/main/java/org/java/lessons/mvc/controller/PizzaController.java
+++ src/main/java/org/java/lessons/mvc/controller/PizzaController.java
@@ -7,10 +7,15 @@ import org.java.lessons.db.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/")
@@ -41,4 +46,31 @@ public class PizzaController {
 		model.addAttribute("pizza", pizza);
 		return "pizza-show";
 	}
+	
+	@GetMapping("/create")
+	public String pizzaCreate(Model model)
+	{
+		model.addAttribute("pizza", new Pizza());
+		return "pizza-create";
+	}
+	
+	@PostMapping("/create")
+	public String pizzaStore(
+				@Valid @ModelAttribute Pizza pizza,
+				BindingResult bindingResult,
+				Model model
+			) {
+		if(bindingResult.hasErrors())
+		{
+			System.out.println("Error:");
+			
+			bindingResult.getAllErrors().forEach(System.out::println);
+			return "pizza-create";
+		}
+		
+		pizzaService.save(pizza);
+		
+		return "redirect:/";
+	}
+	
 }
